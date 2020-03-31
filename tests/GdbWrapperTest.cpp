0ae2e2ac3ef832c49294d3a27938c16f927a7834
Mazen Al-Hagri <mazen.abdulaziz@gmail.com>
2020-04-01 01:02:22 +0200

    Slow work in progress

--- tests/GdbWrapperTest.cpp
+++ tests/GdbWrapperTest.cpp
@@ -1,9 +1,6 @@
-// Let Catch provide main():
-#define CATCH_CONFIG_MAIN
-
+#include "catch2/catch.hpp"
 #include "gdb/GdbWrapper.hpp"
-#include "extern/catch.hpp"
 
 TEST_CASE( "Factorial of 0 is 1 (fail)", "[single-file]" ) {
-  REQUIRE( 1 == 0 );
+  REQUIRE( 1 == 1 );
 }
